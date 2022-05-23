2b3ea71519ac0a23b794664d2ea74ff449fbb9f5
Sultan Sena Acar <senaacaar20@gmail.com>
2022-05-24 00:17:03 +0300

    Giriş Ekranı yapıldı.

---
+++ Lime Go/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Oyunda Çýkýldý");
+        Application.Quit();
+    }
+    public void Back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
